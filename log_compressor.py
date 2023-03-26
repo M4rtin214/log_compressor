60edc40e1d53d5f4f6a9b83e4da524c615f715b4
M4rtin214 <65338409+M4rtin214@users.noreply.github.com>
2023-03-26 10:06:05 +0200

    Update log_compressor.py
    adding information about overwriting the gzip file; better specification of variable

--- log_compressor.py
+++ log_compressor.py
@@ -1,8 +1,9 @@
 #-------------------------------------------------------------------------------
 # Name:        log_compressor
 # Purpose:     Skript projde soubory *.log v "PATH" a zkomprimuje je do gzip
-#              souboru, puvodni soubor nahradni prazdnym souborem
-#              - musi byt spusteno s pravy pro zapis
+#              souboru, puvodni soubor nahradni prazdnym souborem. Prepise 
+#              puvodni gzip soubory.
+#              - vyzaduje prava pro zapis (linux)
 #-------------------------------------------------------------------------------
 
 import gzip
@@ -11,17 +12,17 @@ import os
 PATH = "/var/log"
 
 
-def convert_to_gzip(file_path):
-    with open(file_path, 'rb') as f_in, gzip.open(f"{file_path}.gz", 'wb') as f_out:
+def convert_to_gzip(full_path):
+    with open(full_path, 'rb') as f_in, gzip.open(f"{full_path}.gz", 'wb') as f_out:
         f_out.writelines(f_in)
 
 def main():
     for root, dirs, files in os.walk(PATH):
         for name in files:
             if name.endswith((".log")):
-                file_path = f"{root}/{name}"
-                convert_to_gzip(file_path)
-                with open(file_path, 'w') as f:
+                full_path = f"{root}/{name}"
+                convert_to_gzip(full_path)
+                with open(full_path, 'w') as f:
                     f.write("")
 
 if __name__ == '__main__':
